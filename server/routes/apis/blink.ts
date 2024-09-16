02ba1ab4112bc01e49b6b96f2f52b6e0ce91114d
Joseph Melvin Kanu <kanujosephmelvin@gmail.com>
2024-09-16 18:17:04 +0000

    update

--- server/routes/apis/blink.ts
+++ server/routes/apis/blink.ts
@@ -1,24 +1,17 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import * as controller from "../../app/controllers/blink_controller";
 
 const router = express.Router();
 
-// Define CORS options
 const corsOptions = {
-    origin: '*', 
-    methods: 'GET,POST',
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: "*",
+  methods: "GET,POST",
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-router.use(cors(corsOptions));
+router.get("/:id", controller.getBlink);
 
+router.post("/:id", controller.donate);
 
-router.get('/:id', controller.getBlink);
-
-router.post('/:id', controller.donate);
-
-
-
-
-export default router
+export default router;

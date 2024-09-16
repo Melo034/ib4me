15ac9111511cf602769fc5a7e337af823d2764de
Joseph Melvin Kanu <kanujosephmelvin@gmail.com>
2024-09-16 21:05:48 +0000

    update

--- client/src/Campaigns.tsx
+++ client/src/Campaigns.tsx
@@ -7,7 +7,7 @@ const Campaigns = () => {
   return (
     <>
     <div
-      className="w-full h-auto bg-center bg-no-repeat bg-cover"
+      className="w-full min-h-screen bg-center bg-no-repeat bg-cover"
       style={{ backgroundImage: `url('/images/causes.png')` }}
     >
       <div className="relative z-10 flex justify-center pt-8">
